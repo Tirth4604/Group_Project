507887735be645374cc3439a574069a6b62413fc
Tirth4604 <158247513+Tirth4604@users.noreply.github.com>
2024-04-02 23:41:25 -0400

    Update Game.java

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -2,7 +2,7 @@
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
  * Add your name as an author and the date!
- * tirth/
+ * tirth panchal/
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
